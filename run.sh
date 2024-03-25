a0e4e911c290d706e24ed57bdd95d56adfe7a53c
Liu-rj <11911808@mail.sustech.edu.cn>
2024-03-25 13:40:21 +0000

    update scripts

--- run.sh
+++ run.sh
@@ -12,27 +12,27 @@
 
 
 
-# python prepare_dataset.py --dataset friendster
-# python create_neigh_cache.py --neigh-cache-size 10e9 --dataset friendster
-# sudo env PATH=$PATH python run_ginex.py --dataset friendster --neigh-cache-size 10e9 --feature-cache-size 10e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
-# sudo env PATH=$PATH python run_ginex.py --dataset friendster --neigh-cache-size 10e9 --feature-cache-size 10e9 --sb-size 10000 --num-epochs 3 --verbose --train-only
+python prepare_dataset.py --dataset friendster --path /nvme2n1/offgs_dataset/friendster-offgs
+python create_neigh_cache.py --neigh-cache-size 10e9 --dataset friendster
+sudo env PATH=$PATH python run_ginex.py --dataset friendster --neigh-cache-size 10e9 --feature-cache-size 10e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
+sudo env PATH=$PATH python run_ginex.py --dataset friendster --neigh-cache-size 10e9 --feature-cache-size 10e9 --sb-size 10000 --num-epochs 3 --verbose --train-only
 
-# python create_neigh_cache.py --neigh-cache-size 6e9 --dataset friendster
-# sudo env PATH=$PATH python run_ginex.py --dataset friendster --neigh-cache-size 6e9 --feature-cache-size 6e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
-# sudo env PATH=$PATH python run_ginex.py --dataset friendster --neigh-cache-size 6e9 --feature-cache-size 6e9 --sb-size 10000 --num-epochs 3 --verbose --train-only
-
-
-# python prepare_dataset.py --dataset mag240m
-# python create_neigh_cache.py --neigh-cache-size 20e9 --dataset mag240m
-# sudo env PATH=$PATH python run_ginex.py --dataset mag240m --neigh-cache-size 20e9 --feature-cache-size 20e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
-# sudo env PATH=$PATH python run_ginex.py --dataset mag240m --neigh-cache-size 20e9 --feature-cache-size 20e9 --sb-size 10000 --num-epochs 3 --verbose --train-only
+python create_neigh_cache.py --neigh-cache-size 6e9 --dataset friendster
+sudo env PATH=$PATH python run_ginex.py --dataset friendster --neigh-cache-size 6e9 --feature-cache-size 6e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
+sudo env PATH=$PATH python run_ginex.py --dataset friendster --neigh-cache-size 6e9 --feature-cache-size 6e9 --sb-size 10000 --num-epochs 3 --verbose --train-only
 
 
-# python prepare_dataset.py --dataset ogbn-papers100M
-# python create_neigh_cache.py --neigh-cache-size 10e9 --dataset ogbn-papers100M
+python prepare_dataset.py --dataset mag240m --path /nvme1n1/offgs_dataset/mag240m-offgs
+python create_neigh_cache.py --neigh-cache-size 20e9 --dataset mag240m
+sudo env PATH=$PATH python run_ginex.py --dataset mag240m --neigh-cache-size 20e9 --feature-cache-size 20e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
+sudo env PATH=$PATH python run_ginex.py --dataset mag240m --neigh-cache-size 20e9 --feature-cache-size 20e9 --sb-size 10000 --num-epochs 3 --verbose --train-only
+
+
+python prepare_dataset.py --dataset ogbn-papers100M --path /nvme2n1/offgs_dataset/ogbn-papers100M-offgs
+python create_neigh_cache.py --neigh-cache-size 10e9 --dataset ogbn-papers100M
 sudo env PATH=$PATH python run_ginex.py --dataset ogbn-papers100M --num-hiddens 512 --dropout 0.2 --neigh-cache-size 10e9 --feature-cache-size 10e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
 sudo env PATH=$PATH python run_ginex.py --dataset ogbn-papers100M --num-hiddens 512 --dropout 0.2 --neigh-cache-size 10e9 --feature-cache-size 10e9 --sb-size 10000 --num-epochs 3 --verbose --train-only
 
-# python create_neigh_cache.py --neigh-cache-size 5e9 --dataset ogbn-papers100M
+python create_neigh_cache.py --neigh-cache-size 5e9 --dataset ogbn-papers100M
 sudo env PATH=$PATH python run_ginex.py --dataset ogbn-papers100M --num-hiddens 512 --dropout 0.2 --neigh-cache-size 5e9 --feature-cache-size 5e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
 sudo env PATH=$PATH python run_ginex.py --dataset ogbn-papers100M --num-hiddens 512 --dropout 0.2 --neigh-cache-size 5e9 --feature-cache-size 5e9 --sb-size 10000 --num-epochs 3 --verbose --train-only
