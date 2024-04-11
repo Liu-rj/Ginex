e589b716e4f33e9e1b011875845f39794b738012
Liu-rj <11911808@mail.sustech.edu.cn>
2024-04-11 02:53:01 +0000

    update gat run scripts

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ python prepare_dataset.py --dataset ogbn-papers100M --path /nvme2n1/offgs_datase
 python create_neigh_cache.py --neigh-cache-size 5e9 --dataset ogbn-papers100M
 sudo env PATH=$PATH python run_ginex.py --dataset ogbn-papers100M --num-hiddens 256 --dropout 0.2 --neigh-cache-size 5e9 --feature-cache-size 5e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
 sudo env PATH=$PATH python run_ginex.py --dataset ogbn-papers100M --num-hiddens 256 --dropout 0.2 --neigh-cache-size 5e9 --feature-cache-size 5e9 --sb-size 10000 --num-epochs 3 --verbose --train-only --model SAGE
-sudo env PATH=$PATH python run_ginex.py --dataset ogbn-papers100M --num-hiddens 256 --dropout 0.2 --neigh-cache-size 5e9 --feature-cache-size 5e9 --sb-size 10000 --num-epochs 3 --verbose --train-only --model GAT
+sudo env PATH=$PATH python run_ginex.py --dataset ogbn-papers100M --num-hiddens 64 --dropout 0.2 --neigh-cache-size 5e9 --feature-cache-size 5e9 --sb-size 10000 --num-epochs 3 --verbose --train-only --model GAT
 
 
 # MAG240M
@@ -24,7 +24,7 @@ python prepare_dataset.py --dataset mag240m --path /nvme1n1/offgs_dataset/mag240
 python create_neigh_cache.py --neigh-cache-size 10e9 --dataset mag240m
 sudo env PATH=$PATH python run_ginex.py --dataset mag240m --num-hiddens 256 --dropout 0.2 --neigh-cache-size 10e9 --feature-cache-size 10e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
 sudo env PATH=$PATH python run_ginex.py --dataset mag240m --num-hiddens 256 --dropout 0.2 --neigh-cache-size 10e9 --feature-cache-size 10e9 --sb-size 10000 --num-epochs 3 --verbose --train-only --model SAGE
-sudo env PATH=$PATH python run_ginex.py --dataset mag240m --num-hiddens 256 --dropout 0.2 --neigh-cache-size 10e9 --feature-cache-size 10e9 --sb-size 10000 --num-epochs 3 --verbose --train-only --model GAT
+sudo env PATH=$PATH python run_ginex.py --dataset mag240m --num-hiddens 64 --dropout 0.2 --neigh-cache-size 10e9 --feature-cache-size 10e9 --sb-size 10000 --num-epochs 3 --verbose --train-only --model GAT
 
 
 # Friendster
@@ -32,7 +32,7 @@ python prepare_dataset.py --dataset friendster --path /nvme2n1/offgs_dataset/fri
 python create_neigh_cache.py --neigh-cache-size 4e9 --dataset friendster
 sudo env PATH=$PATH python run_ginex.py --dataset friendster --num-hiddens 256 --dropout 0 --neigh-cache-size 4e9 --feature-cache-size 4e9 --sb-size 10000 --num-epochs 1 --verbose --train-only --sample-mode
 sudo env PATH=$PATH python run_ginex.py --dataset friendster --num-hiddens 256 --dropout 0 --neigh-cache-size 4e9 --feature-cache-size 4e9 --sb-size 10000 --num-epochs 3 --verbose --train-only --model SAGE
-sudo env PATH=$PATH python run_ginex.py --dataset friendster --num-hiddens 256 --dropout 0 --neigh-cache-size 4e9 --feature-cache-size 4e9 --sb-size 10000 --num-epochs 3 --verbose --train-only --model GAT
+sudo env PATH=$PATH python run_ginex.py --dataset friendster --num-hiddens 64 --dropout 0 --neigh-cache-size 4e9 --feature-cache-size 4e9 --sb-size 10000 --num-epochs 3 --verbose --train-only --model GAT
 
 
 # IGB-HOM
@@ -40,4 +40,4 @@ python prepare_dataset.py --dataset igb-full --path /nvme1n1/offgs_dataset/igb-f
 python create_neigh_cache.py --neigh-cache-size 15e9 --dataset igb-full
 sudo env PATH=$PATH python run_ginex.py --dataset igb-full --num-hiddens 256 --dropout 0 --neigh-cache-size 15e9 --feature-cache-size 15e9 --sb-size 20000 --num-epochs 1 --verbose --train-only --sample-mode
 sudo env PATH=$PATH python run_ginex.py --dataset igb-full --num-hiddens 256 --dropout 0 --neigh-cache-size 15e9 --feature-cache-size 15e9 --sb-size 20000 --num-epochs 3 --verbose --train-only --model SAGE
-sudo env PATH=$PATH python run_ginex.py --dataset igb-full --num-hiddens 256 --dropout 0 --neigh-cache-size 15e9 --feature-cache-size 15e9 --sb-size 20000 --num-epochs 3 --verbose --train-only --model GAT
+sudo env PATH=$PATH python run_ginex.py --dataset igb-full --num-hiddens 64 --dropout 0 --neigh-cache-size 15e9 --feature-cache-size 15e9 --sb-size 20000 --num-epochs 3 --verbose --train-only --model GAT
